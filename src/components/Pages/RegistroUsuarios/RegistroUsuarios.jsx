8f86865f622fc79be5590f1ffc40d9bf3b61f6c8
Santiago Castaño <santiagocastano589@gmail.com>
2025-02-13 16:51:50 -0500

    Estilos de formulario usuario

--- src/components/Pages/RegistroUsuarios/RegistroUsuarios.jsx
+++ src/components/Pages/RegistroUsuarios/RegistroUsuarios.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Header } from '../../Layouts/Header/Header'
 import './RegistroUsuarios.css';
+import LogoR from '../../../assets/icon.png';
 
 
 export const RegistroUsuarios = () => {
@@ -11,42 +12,42 @@ export const RegistroUsuarios = () => {
             <div className="form-box">
                 <form action="registro.php" method="post" className="input-group" id="form">
                     <center>
-                         <img class="loguito2" src="imagenes/logo.png" alt="" />
+                         <img className="loguito2" src={LogoR} alt="Logo" />
                     </center>
 
                         <label for="Codigo">Identificación</label>
-                        <input type="text" class="input-field" placeholder="Identificación" name="identificacion"/>
+                        <input type="text" className="input-field" placeholder="Identificación" name="identificacion"/>
 
                         <label for="nombre">Nombre</label>
-                        <input type="text" class="input-field" placeholder="Nombre" name="nombre"/>
+                        <input type="text" className="input-field" placeholder="Nombre" name="nombre"/>
 
                         <label for="fecha_nacimiento">Fecha Nacimiento</label>
-                        <input type="date" class="input-field" name="fecha_nacimiento"/>
+                        <input type="date" className="input-field" name="fecha_nacimiento"/>
 
                         <label for="correo">Correo</label>
-                        <input type="text" class="input-field" placeholder="Correo" name="correo"/>
+                        <input type="text" className="input-field" placeholder="Correo" name="correo"/>
 
                         <label for="contrasena">Contraseña</label>
-                        <input type="password" class="input-field" placeholder="Contraseña" name="contrasena"/>
+                        <input type="password" className="input-field" placeholder="Contraseña" name="contrasena"/>
 
                         <label for="sexo">Sexo</label>
-                            <select class="input-field" name="sexo">
+                            <select className="input-field" name="sexo">
                             <option value="Masculino">Masculino</option>
                             <option value="Femenino">Femenino</option>
                             <option value="Otro">Otro Género</option>
                         </select>
 
                         <label for="deporte">Deporte</label>
-                        <select class="input-field" name="deporte">
+                        <select className="input-field" name="deporte">
                             <option value="Baloncesto">Baloncesto</option>
                             <option value="Voleibol">Voleibol</option>
                             <option value="Futbol">Futbol</option>
                         </select>
 
                         <label for="telefono">Teléfono</label>
-                        <input type="text" class="input-field" placeholder="Teléfono" name="telefono"/>
+                        <input type="text" className="input-field" placeholder="Teléfono" name="telefono"/>
                     
-                        <button type="submit" class="submit-btn">Registrarse</button>
+                        <button type="submit" className="submit-btn">Registrarse</button>
                 </form>
             </div>
         </div>
